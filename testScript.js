ceada4227dce82fde69d44e6d1c57d7f686b49da
rbstina <94511543+rbstina@users.noreply.github.com>
2024-03-10 14:34:34 +0930

    Add files via upload

---
+++ testScript.js
@@ -0,0 +1,5 @@
+
+  document.getElementById('button').addEventListener('click', function() {
+    document.getElementById('message').textContent = 'Too close!';
+  });
+// 3/3 part of flag : forinternship@ssrd}
